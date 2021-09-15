2510ac93e74816169bb52918d761cafb86407f60
taylfw88 <frank.w.taylor88@gmail.com>
2021-09-15 13:57:38 -0400

    Added some shadowing to portnav and nav bar.

--- src/components/PortNav.jsx
+++ src/components/PortNav.jsx
@@ -5,11 +5,21 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 function PortNav() {
   return (
     <div className="container">
-      <a href="/numberGen/index.html">Number Generator</a>
-      <a href="backgroundButton/index.html">Background Generator</a>
-      <a href="/snake/index.html">Snake</a>
-      <a href="https://frank-taylor-retrodraw.netlify.app/">Retro Draw!</a>
-      <a href="/Calculator/index.html">Calculator</a>
+      <a href="/numberGen/index.html" className="link">
+        Number Generator
+      </a>
+      <a href="backgroundButton/index.html" className="link">
+        Background Generator
+      </a>
+      <a href="/snake/index.html" className="link">
+        Snake
+      </a>
+      <a href="https://frank-taylor-retrodraw.netlify.app/" className="link">
+        Retro Draw!
+      </a>
+      <a href="/Calculator/index.html" className="link">
+        Calculator
+      </a>
     </div>
   );
 }

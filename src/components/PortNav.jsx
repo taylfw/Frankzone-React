92430bb8267f0d0779abed376aacdd65dd35a228
taylfw88 <frank.w.taylor88@gmail.com>
2022-02-14 06:38:23 -0500

    added some more to portfolio

--- src/components/PortNav.jsx
+++ src/components/PortNav.jsx
@@ -4,22 +4,33 @@ import reactDom from "react-dom";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 function PortNav() {
   return (
-    <div className="container">
-      <a href="/numberGen/index.html" className="link">
-        Number Generator
-      </a>
-      <a href="backgroundButton/index.html" className="link">
-        Background Generator
-      </a>
-      <a href="/snake/index.html" className="link">
-        Snake
-      </a>
-      <a href="https://frank-taylor-retrodraw.netlify.app/" className="link">
-        Retro Draw!
-      </a>
-      <a href="/Calculator/index.html" className="link">
-        Calculator
-      </a>
+    <div>
+      <div className="container">
+        <a href="/numberGen/index.html" className="link">
+          Number Generator
+        </a>
+        <a href="backgroundButton/index.html" className="link">
+          Background Generator
+        </a>
+        <a href="/snake/index.html" className="link">
+          Snake
+        </a>
+        <a href="https://frank-taylor-retrodraw.netlify.app/" className="link">
+          Retro Draw!
+        </a>
+        <a href="/Calculator/index.html" className="link">
+          Calculator
+        </a>
+      </div>
+
+      <div className="container">
+        <a href="https://grocerylisthacker.herokuapp.com/" className="link">
+          Grocery List Combinator
+        </a>
+        <a href="https://sleepytime-earplugs.herokuapp.com/" className="link">
+          Sleepy Time Earplugs
+        </a>
+      </div>
     </div>
   );
 }
